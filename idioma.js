fb1025c719a8b7322f83895f7d61e3959b355279
brennodelucas <brennobnsvh@gmail.com>
2025-05-22 19:17:17 -0300

    idioma 1.2
    verificando se há um idioma no LS. pegando idioma atual do navegador ao atualizar

--- idioma.js
+++ idioma.js
@@ -20,4 +20,32 @@ function aplicarIdioma(idioma){
         if(textos[idioma][chave]){
             el.textContent = textos[idioma][chave]        }
     })
+
+    const seletorIdioma = document.getElementById('idioma')
+
+    let idioma = localStorage.getItem('idioma')
+
+    // Se não tiver salvo, detecta o idioma do navegador
+    if(!idioma){
+        const idiomaAtual = navigator.language.slice(0,2)
+
+        // Se for 'pt', mantém; senão, coloca 'en' como padrão
+        idioma =  idiomaAtual === 'pt'? 'pt' : 'en'
+    }
+
+    // Ajusta o <select> de idiomas na interface
+    seletorIdioma.value = idioma
+
+    aplicarIdioma(idioma)
+
+    seletorIdioma.addEventListener('change', () =>{
+        // Pega o novo idioma selecionado
+        const novoIdioma = seletorIdioma.value
+
+        // Aplica o novo idioma na página
+        aplicarIdioma(novoIdioma)
+
+        // Salva a escolha para lembrar depois
+        localStorage.setItem('idioma', novoIdioma)
+    })
 }
