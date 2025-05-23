c9b6f0f9ead256c906b712575fd2721dd6c96127
brennodelucas <brennobnsvh@gmail.com>
2025-05-22 21:45:31 -0300

    idiomas
    adicionando traduçoes

--- idioma.js
+++ idioma.js
@@ -6,10 +6,11 @@ const textos = {
         menu: "menu",
         review: "avaliaçoes",
         adress: "endereço",
+        Bemvindo: "Seja bem-vindo à nossa cafeteria!Aqui, você encontra um ambiente acolhedor e perfeito para saborear cafés especiais, chás aromáticos e deliciosos quitutes preparados com muito carinho. Nosso espaço foi pensado para proporcionar conforto e boas experiências, seja para um momento de tranquilidade, um encontro com amigos ou uma reunião de trabalho.",
         pegue: "Pegue o seu agora",
         nos: "sobre nós",
         cafeEspecial: "O que faz nosso café ser especial?",
-        nossaCafeteria: "",
+        nossaCafeteria: "Na nossa cafeteria, cada xícara conta uma história. Aqui você encontra muito mais do que cafés especiais: oferecemos um espaço acolhedor para você relaxar, trabalhar, encontrar amigos ou simplesmente apreciar um momento só seu. Trabalhamos com grãos selecionados de alta qualidade, preparados por baristas apaixonados pelo que fazem. Do espresso intenso ao cappuccino cremoso, cada bebida é feita com carinho para proporcionar uma experiência única. Além disso, nosso cardápio conta com opções irresistíveis de bolos, pães artesanais, lanches e doces que combinam perfeitamente com o café.",
         saiba: "saiba mais"
 
     },
@@ -21,11 +22,12 @@ const textos = {
         menu: "menu",
         review: "review",
         adress: "addres",
+        Bemvindo: "Welcome to our coffee shop! Here, you will find a cozy and perfect environment to enjoy specialty coffees, aromatic teas and delicious treats prepared with great care. Our space was designed to provide comfort and good experiences, whether for a moment of tranquility, a meeting with friends or a business meeting.",
         pegue: "Get yours now",
         nos: "about us",
         cafeEspecial: "What makes our coffee special?",
-        nossaCafeteria: "",
-        saiba: "know us"
+        nossaCafeteria: "In our coffee shop, every cup tells a story. hete, you will find much more than just speciality coffees. we offer a welcoming space for you to relax, work, meet friends or simply enjoy some time alone. We work with selected, high-quality beans, prepared by baristas who are passionate about what they do. From intense espresso to creamy cappuccino, each drink is made with care to provide a unique experience, in addition, our menu features irresistible options of cakes, artisanal breads, snacks and sweets that pair perfectly with coffee.",
+        saiba: "know us",
     },
 
     fr: {
@@ -35,10 +37,11 @@ const textos = {
         menu: "menu",
         review: "revoir",
         adress: "adresse",
+        Bemvindo: "Bienvenue dans notre café! Ici, vous rencontrez une ambiance agréable et parfaite pour déguster des cafés spéciaux, des produits aromatiques et délicieux préparés avec beaucoup de sucre. Notre espace a été pensé pour offrir du confort et de belles expériences, seja pour un moment de tranquillité, une rencontre avec des amis ou une réunion de travail.",
         pegue: "Obtenez le vôtre!",
         nos: "à propos de nous",
         cafeEspecial: "Qu'est-ce qui rend notre café spécial ?",
-        nossaCafeteria: "",
+        nossaCafeteria: "Dans notre cafétéria, chaque fois, il y a une histoire. Ici, vous rencontrez bien plus que les cafés spéciaux : nous offrons un espace agréable pour vous détendre, travailler, rencontrer des amis ou simplement apprécier un moment comme vous. Nous travaillons avec de grands sélectionnés de haute qualité, préparés par les baristas apaixonados pour que cela se fasse. Faire un expresso intense avec un cappuccino cremoso, chaque boisson est faite avec du café pour offrir une expérience unique. En outre, nous avons des cartes avec des options irrésistibles de bols, de restaurants artisanaux, de restaurants et de cours qui se combinent parfaitement avec le café.",
         saiba: "apprendre encore plus"
     }
 }
