6314263fee48331872ff7567dd0b71b7f0a7a44d
brennodelucas <brennobnsvh@gmail.com>
2025-05-22 21:25:23 -0300

    idioma 1.3
    separando estruturas e adicionando traduçoes

--- idioma.js
+++ idioma.js
@@ -1,10 +1,45 @@
 const textos = {
     pt: {
-        titulo: "code com o melhor café da região!"
+        titulo: "code com o melhor café da região!",
+        home: "inicio",
+        sobre: "sobre",
+        menu: "menu",
+        review: "avaliaçoes",
+        adress: "endereço",
+        pegue: "Pegue o seu agora",
+        nos: "sobre nós",
+        cafeEspecial: "O que faz nosso café ser especial?",
+        nossaCafeteria: "",
+        saiba: "saiba mais"
+
     },
 
     en: {
-        titulo: "code with the best coffee in the region!"
+        titulo: "code with the best coffee in the region!",
+        home: "home",
+        sobre: "about",
+        menu: "menu",
+        review: "review",
+        adress: "addres",
+        pegue: "Get yours now",
+        nos: "about us",
+        cafeEspecial: "What makes our coffee special?",
+        nossaCafeteria: "",
+        saiba: "know us"
+    },
+
+    fr: {
+        titulo: "code avec le meilleur café de la région !",
+        home: "commencer",
+        sobre: "sur",
+        menu: "menu",
+        review: "revoir",
+        adress: "adresse",
+        pegue: "Obtenez le vôtre!",
+        nos: "à propos de nous",
+        cafeEspecial: "Qu'est-ce qui rend notre café spécial ?",
+        nossaCafeteria: "",
+        saiba: "apprendre encore plus"
     }
 }
 
@@ -18,9 +53,10 @@ function aplicarIdioma(idioma){
 
         // Se houver uma tradução para esta chave, troca o conteúdo
         if(textos[idioma][chave]){
-            el.textContent = textos[idioma][chave]        }
+            el.textContent = textos[idioma][chave]       
+         }
     })
-
+}
     const seletorIdioma = document.getElementById('idioma')
 
     let idioma = localStorage.getItem('idioma')
@@ -28,9 +64,10 @@ function aplicarIdioma(idioma){
     // Se não tiver salvo, detecta o idioma do navegador
     if(!idioma){
         const idiomaAtual = navigator.language.slice(0,2)
+        const idiomasSuportados = ['pt', 'en', 'fr']
 
         // Se for 'pt', mantém; senão, coloca 'en' como padrão
-        idioma =  idiomaAtual === 'pt'? 'pt' : 'en'
+        idioma =  idiomasSuportados.includes(idiomaAtual) ? idiomaAtual : 'pt'
     }
 
     // Ajusta o <select> de idiomas na interface
@@ -48,4 +85,3 @@ function aplicarIdioma(idioma){
         // Salva a escolha para lembrar depois
         localStorage.setItem('idioma', novoIdioma)
     })
-}
